dd598884ee6ac3babba67df50eec2d05a254bf5c
User <theoffmouse@gmail.com>
2020-02-10 16:19:49 +0200

    Modified main.java file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.printf("Hello!");
+        System.out.printf("How are you?");
     }
 }
